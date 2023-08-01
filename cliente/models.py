0f12f5ff9e783b0efb2644bcfeb73bac09dfd861
Gabriel Henrique Alves Dias <gabriel688dias@hotmail.com>
2023-08-01 13:06:31 -0300

    cliente

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cliente(models.Model):
+    
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+
+    
+    def __str__(self):
+        return self.nome
+    
+
+    
+class Agendamento(models.Model):
+    
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateField()
+    horario = models.TimeField()
+    
+    def __str__(self):
+        return self.cliente.nome 
+    
